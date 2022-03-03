ba7525362bbdddde407aba63b01e9d003e043ca6
nGyu <vpdls1511@gmail.com>
2022-03-03 23:30:04 +0900

    project initialize

---
+++ src/main/java/com/example/dongdong_web_app/DongdongWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.dongdong_web_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DongdongWebAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DongdongWebAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/dongdong_web_app/DongdongWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dongdong_web_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DongdongWebAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
