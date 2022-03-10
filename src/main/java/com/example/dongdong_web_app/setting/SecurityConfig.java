9359ac9826ce85436ef464cb2311c0f74a99a373
nGyu <vpdls1511@gmail.com>
2022-03-10 14:54:45 +0900

    jwt 적용중 // Spring Security 설정중

--- src/main/java/com/example/dongdong_web_app/setting/SecurityConfig.java
+++ src/main/java/com/example/dongdong_web_app/setting/SecurityConfig.java
@@ -2,20 +2,77 @@ package com.example.dongdong_web_app.setting;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.SpringSecurityMessageSource;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final TokenProvider tokenProvider;
+    private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+    private final JwtAccessDeniedHandler jwtAccessDeniedHandler;
+
+
+    public SecurityConfig(
+            TokenProvider tokenProvider,
+            CorsFilter corsFilter,
+            JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint,
+            JwtAccessDeniedHandler jwtAccessDeniedHandler
+    ) {
+        this.tokenProvider = tokenProvider;
+        this.jwtAuthenticationEntryPoint = jwtAuthenticationEntryPoint;
+        this.jwtAccessDeniedHandler = jwtAccessDeniedHandler;
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception{
-        http.csrf().disable();
+        Object corsFilter;
+        http.csrf().disable()
+                .cors().configurationSource(corsConfigurationSource()) // CORS 환경설정 적용
+                .addFilterBefore(corsFilter, UsernamePasswordAuthenticationFilter.class) // Custom Jwt 토큰 필터를 filter chain의 UsernamePasswordAuthenticationFilter 앞에 세팅
+                .exceptionHandling() // 예외처리 기능 수행
+                .authenticationEntryPoint(jwtAuthenticationEntryPoint) // 인증 실패 진입점
+                .accessDeniedHandler(jwtAccessDeniedHandler) // 인가 실패 진입점
+
+                .and() // 세션을 사용하지 않으므로 STATELESS로 설정
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+
+                .and() // 로그인, refreshToken을 통한 AccessToken 재발급 요청, api 명세서 관련을 제외하고 모든 요청은 인증이 필요합니다.
+                .authorizeRequests()
+                .antMatchers("/api/authenticate").permitAll()
+                .antMatchers("/api/reissue").permitAll()
+                .antMatchers("/api/docs/api-doc.html").permitAll()
+                .anyRequest().authenticated()
+
+                .and()
+                .apply(new JwtSecurityConfig(tokenProvider));;
+    }
+
+    @Bean
+    private CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+
+        configuration.addAllowedOrigin("{hostURL:frontEndPort}");
+        configuration.addAllowedHeader("*");
+        configuration.addAllowedMethod("*");
+        configuration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/api/**", configuration);
+        return source;
     }
 
     @Bean
