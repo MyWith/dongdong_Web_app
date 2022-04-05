98c7da14e742e7df686b6b6a63c2580757457c11
nGyu <vpdls1511@gmail.com>
2022-04-06 00:02:11 +0900

    유저의 정보를 업데이트하는 테스트 코드 작성 ( 테스트 전 )

--- src/test/java/com/example/dongdong_web_app/auth/UpdateLocation.java
+++ src/test/java/com/example/dongdong_web_app/auth/UpdateLocation.java
@@ -1,8 +1,26 @@
 package com.example.dongdong_web_app.auth;
 
+import com.example.dongdong_web_app.application.auth.entity.AuthEntity;
+import com.example.dongdong_web_app.application.auth.repository.AuthRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Optional;
 
 @SpringBootTest
 public class UpdateLocation {
-    
+
+    @Autowired
+    AuthRepository authRepository;
+
+    @Test
+    public void 유저위치정보업데이트(){
+        Optional<AuthEntity> user = authRepository.findById("1");
+        user.ifPresent(selectUser -> {
+            selectUser.setUserLat(14.12124123);
+            selectUser.setUserLon(14.12224421);
+            authRepository.save(selectUser);
+        });
+    }
 }
