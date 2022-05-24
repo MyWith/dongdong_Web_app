179d17cd4ca0bde95e349fab570316f973e9b651
nGyu <vpdls1511@gmail.com>
2022-05-25 08:59:44 +0900

    refactor(nGyu) : 서버주소 및 정보 yml로 관리 가능

--- src/main/java/com/example/dongdong_web_app/application/auth/service/email/EmailService.java
+++ src/main/java/com/example/dongdong_web_app/application/auth/service/email/EmailService.java
@@ -4,6 +4,7 @@ import com.example.dongdong_web_app.application.auth.entity.EmailAuth;
 import com.example.dongdong_web_app.application.auth.repository.CheckCustomRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
@@ -13,37 +14,65 @@ import org.springframework.security.access.AccessDeniedException;
 import org.springframework.stereotype.Service;
 
 import javax.mail.Message;
+import javax.mail.Session;
+import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 import javax.transaction.Transactional;
 import java.time.LocalDateTime;
+import java.util.Properties;
 
 @Service
 @EnableAsync
 @RequiredArgsConstructor
 public class EmailService {
     private static final String EMAIL_CHECK_SUBJECT_COMMENT = "[마이윗] 이메일 인증";
+    private static final String FROM_NAME = "마이윗";
+    private static final int PORT = 587;
+
+    @Value("${config.host}")
+    private String URL;
+    @Value("${spring.mail.host}")
+    private String HOST;
+    @Value("${spring.mail.username}")
+    private String FROM;
+    @Value("${spring.mail.username}")
+    private String SMTP_USERNAME;
+    @Value("${spring.mail.password}")
+    private String SMTP_PASSWORD;
+
 
     @Autowired
     private CheckCustomRepository checkCustomRepository;
 
-    @Autowired
-    private JavaMailSender javaMailSender;
-
     @Async
     public void send(final String email, final String authToken) {
-        MimeMessage message = javaMailSender.createMimeMessage();
-        String htmlMsg = "<a href=\"http://localhost:8080/api/auth/check/confirm-email?email=" + email + "&authToken=" + authToken + "\">인증하기</a>";
+        Properties props = System.getProperties();
 
-        try{
+        props.put("mail.transport.protocol", "smtp");
+        props.put("mail.smtp.port", PORT);
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.auth", "true");
+
+        String htmlMsg = "<a href=\"http://" + URL + ":8080/api/auth/check/confirm-email?email=" + email + "&authToken=" + authToken + "\">인증하기</a>";
+
+        Session session = Session.getDefaultInstance(props);
+        MimeMessage message = new MimeMessage(session);
+
+        try {
+            message.setFrom(new InternetAddress(FROM, FROM_NAME));
+            message.setRecipient(Message.RecipientType.TO, new InternetAddress(email));
             message.setSubject(EMAIL_CHECK_SUBJECT_COMMENT);
-            message.setText(htmlMsg, "UTF-8","html");
-            message.setFrom(new InternetAddress("ngyu.test@gmail.com"));
-            message.setRecipients(Message.RecipientType.TO, email);
+            message.setContent(htmlMsg, "text/html;charset=euc-kr");
 
-            javaMailSender.send(message);
-            System.out.println(email + "SEND TO MAIL");
-        }catch (Exception err){
+            System.out.println("Sending...");
+
+            Transport transport = session.getTransport();
+            transport.connect(HOST, SMTP_USERNAME, SMTP_PASSWORD);
+            transport.sendMessage(message, message.getAllRecipients());
+
+            System.out.println("Email sent!");
+        } catch (Exception err) {
             throw new RuntimeException(err);
         }
     }
